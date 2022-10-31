d4c3fc5cf2ae101873b43b631b085c8775b9b39b
Jerry Lott <jj@Jerrys-Mac-mini.local>
2022-10-30 23:32:46 -0500

    Added layout for schedues for employees

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import Employee from '../components/Employee';
+import data from '../utils/data';
 
 export default function Home() {
-  return (
-    <Layout title="Home">Home Page</Layout>
-  )
+	return (
+		<Layout title="Home">
+			<div className="flex flex-col gap-5 mt-10">
+				<table class="table-auto border-spacing-6 border-separate">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Position</th>
+							<th>Sunday</th>
+							<th>Monday</th>
+							<th>Tuesday</th>
+							<th>Wendsday</th>
+							<th>Thursday</th>
+							<th>Friday</th>
+							<th>Saturday</th>
+							<th>Hours</th>
+						</tr>
+					</thead>
+					<tbody>
+						{data.employees.map((employee) => (
+							<Employee employee={employee} key={employee.slug}></Employee>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</Layout>
+	);
 }
---
+++ utils/data.js
@@ -0,0 +1,32 @@
+const data = {
+	employees: [
+		{
+			name: 'Jerry',
+			slug: 'Jerry',
+			position: 'Assistant Manager',
+			sunday: '12:30pm to 9:30pm',
+			monday: '12:30pm to 9:30pm',
+			tuesday: '12:30pm to 9:30pm',
+			wendsday: '6:00am to 3:00pm',
+			thursday: '6:00am to 3:00pm',
+			friday: 'OFF',
+			saturday: 'OFF',
+			hours: '40',
+		},
+		{
+			name: 'Gavin',
+			slug: 'Gavin',
+			position: 'Stocker',
+			sunday: '7:00am to 4:00pm',
+			monday: '3:30pm to 9:30pm',
+			tuesday: '3:30pm to 9:30pm',
+			wendsday: 'OFF',
+			thursday: 'OFF',
+			friday: 'OFF',
+			saturday: '3:30pm to 9:30pm',
+			hours: '26',
+		},
+	],
+};
+
+export default data;
