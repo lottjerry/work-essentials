915c02c5155dbd0cbf948c5d21d49cecedbc7567
Jerry Lott <jj@Jerrys-Mac-mini.local>
2022-11-09 00:01:16 -0600

    Fixed index issue

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import Schedule from '../components/Schedule';
+
+export default function Home() {
+	return (
+		<Layout title="Home">
+			<Schedule />
+		</Layout>
+	);
+}
